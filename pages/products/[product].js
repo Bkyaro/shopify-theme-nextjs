085f1cf0ad32454dd64a9b2a2f7f69cda52b3c8c
Jackal_lzt <bkyarokun@gmail.com>
2024-09-04 14:03:34 +0800

    feat: product page basic structure

---
+++ pages/products/[product].js
@@ -0,0 +1,36 @@
+import { getALlProducts, getProduct } from "../../lib/shopify";
+import ProductPageContent from "../../components/ProductPageContent";
+
+export default function ProductPage({ product }) {
+	return (
+		<div>
+			<ProductPageContent product={product} />
+		</div>
+	);
+}
+
+export async function getStaticPaths() {
+	const products = await getALlProducts();
+
+	const paths = products.map((item) => {
+		const product = String(item.node.handle);
+		return {
+			params: { product },
+		};
+	});
+
+	return {
+		paths: paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	const product = await getProduct(params.product);
+
+	return {
+		props: {
+			product,
+		},
+	};
+}
