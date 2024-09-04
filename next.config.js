9c8c90109dbc5d21a1e9d5d8d44c4f6ac9f4167d
Jackal_lzt <bkyarokun@gmail.com>
2024-09-04 10:33:37 +0800

    feat: init project with storefront api

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	env: {
+		SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+			process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+		SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+	},
+	images: {
+		domains: ["cdn.shopify.com"],
+	},
+};
