0279e4cfc5dce46c7081c761b647c6fc0e524035
Jackal_lzt <bkyarokun@gmail.com>
2024-09-11 17:19:09 +0800

    feat: grab product from collection for recommened section

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import ProductForm from "./ProductForm";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar } from "swiper/modules";
-
+import RecommendedList from "./RecommendedList";
 export default function ProductPageContent({ product }) {
 	console.log("product", product);
 	const images = [];
@@ -20,26 +20,35 @@ export default function ProductPageContent({ product }) {
 	});
 
 	return (
-		<div className="w-full flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl  max-auto">
-			<div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
-				<div className="relative h-96 w-full">
-					<Swiper
-						style={{
-							"--swiper-navigation-color": "#000",
-							"--swiper-pagination-color": "#000",
-						}}
-						navigation={true}
-						pagination={true}
-						scrollbar={true}
-						className="h-96 rounded-2xl"
-						loop={true}
-						modules={[Navigation, Pagination, Scrollbar]}
-					>
-						{images}
-					</Swiper>
+		<div>
+			<div className="w-full flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl  max-auto">
+				<div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
+					<div className="relative h-96 w-full">
+						<Swiper
+							style={{
+								"--swiper-navigation-color": "#000",
+								"--swiper-pagination-color": "#000",
+							}}
+							navigation={true}
+							pagination={true}
+							scrollbar={true}
+							className="h-96 rounded-2xl"
+							loop={true}
+							modules={[Navigation, Pagination, Scrollbar]}
+						>
+							{images}
+						</Swiper>
+					</div>
 				</div>
+				<ProductForm product={product} />
 			</div>
-			<ProductForm product={product} />
+			<p className="py-16 space-y-8 md:space-x-4 lg:sapce-x-8 max-w-3xl w-11/12 mx-auto text-center">
+				{product.description}
+			</p>
+			<RecommendedList
+				current={product.id}
+				products={product.collections.edges[0].node.products.edges}
+			/>
 		</div>
 	);
 }
---
+++ components/RecommendedList.js
@@ -0,0 +1,22 @@
+import ProductCard from "./ProductCard";
+
+const RecommendedList = ({ products, current }) => {
+	return (
+		<div className="bg-white">
+			<div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+				<h2 className="text-2xl font-extrabold text-gray-900 mb-6">
+					Recommended Products
+				</h2>
+				<div className=" text-gray-900 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 lg:gap-x-8">
+					{products.map((product) =>
+						product.node.id === current ? null : (
+							<ProductCard key={product.id} product={product} />
+						)
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RecommendedList;
