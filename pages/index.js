4b1e14909fe8a3c251f4850ce620ab1d8f50e5e2
Jackal_lzt <bkyarokun@gmail.com>
2024-09-12 15:22:51 +0800

    feat: homepage hero section ver1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import { getProductsInCollection } from "../lib/shopify";
 import ProductList from "../components/ProductList";
+import Hero from "../components/Hero";
 
 export default function Home({ products }) {
-	return <ProductList products={products} />;
+	return (
+		<div className="">
+			<Hero />
+			<ProductList products={products} />
+		</div>
+	);
 }
 
 export async function getStaticProps() {
