a2eea1889c2347522311fa0d9de6a0052c6e5e20
Jackal_lzt <bkyarokun@gmail.com>
2024-09-13 16:25:20 +0800

    fix: fix product page with styling

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -21,7 +21,7 @@ export default function ProductPageContent({ product }) {
 
 	return (
 		<div>
-			<div className="w-full flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl  max-auto">
+			<div className="w-full flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8  max-auto sm: px-4">
 				<div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
 					<div className="relative h-96 w-full">
 						<Swiper
